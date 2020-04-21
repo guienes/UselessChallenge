66c0d4ad5d9ce2c12aa7ab389d893868d187647a
Guilherme Enes <guilherme.epecci@gmail.com>
2020-04-21 17:42:42 -0300

    adicionado pop up, bugs arrumados e novos assets.

--- UselessRoullete/UselessRoullete/ViewController.swift
+++ UselessRoullete/UselessRoullete/ViewController.swift
@@ -12,15 +12,25 @@ class ViewController: UIViewController {
     
     let rollButton = UIButton()
     let beginImage = UIImage(named: "Roleta-0")
+    
+    let kacaImage = UIImage(named: "kaca")
+    let eloImage = UIImage(named: "eloisa")
+    let fabricioImage = UIImage(named: "fabricio")
+    let joaoImage = UIImage(named: "joao")
+    let enesImage = UIImage(named: "enes")
+    let luizImage = UIImage(named: "luiz" )
+    let gabrielImage = UIImage(named: "gabriel")
+    let leoImage = UIImage(named: "leo")
+    
     lazy var roulleteImageView = UIImageView(image: beginImage)
-    
-    var numberOfTries: Int = 0
-    
     
     var textField = UILabel()
     
+    
+    
     var roulleteImages: [UIImage] = []
-
+    var roulleteResultImages: [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -33,7 +43,7 @@ class ViewController: UIViewController {
         textField.font = UIFont(name: "Helvetica", size: 20)
         
         textField.textColor = .black
-        textField.text = "Rode e veja sua sorte.."
+        textField.text = "Gire a roleta e encontre pessoas lindas"
         self.view.addSubview(textField)
         
         //posiciona a roleta
@@ -43,13 +53,14 @@ class ViewController: UIViewController {
         self.view.addSubview(roulleteImageView)
         
         roulleteImages = createImageArray(total: 8, imagePrefix: "Roleta")
+        roulleteResultImages = createImageArray(total: 7, imagePrefix: "resultado")
         
         //posiciona o botão
         
         rollButton.setTitle("Gire", for: .normal)
-        rollButton.setTitleColor(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), for: .normal)
+        rollButton.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
         rollButton.layer.cornerRadius = 5
-        rollButton.backgroundColor = .orange
+        rollButton.backgroundColor = .black
         rollButton.addTarget(self, action: #selector(rollButtonFunc), for: .touchUpInside)
         
         rollButton.frame = CGRect(x: self.view.frame.width/2.5, y: self.view.frame.height/1.3, width: 100, height: 70)
@@ -84,20 +95,45 @@ class ViewController: UIViewController {
         }) { (finished) in
             let seconds = 4.97
             DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
-                imageView.image = images.randomElement()
-                if self.numberOfTries == 0 {
-                    self.textField.text = "Você ganhou um grande nada!"
-                    self.numberOfTries += 1
+                imageView.image = self.roulleteResultImages.randomElement()
+                if imageView.image == self.roulleteResultImages[0]{
+                    self.createAlert(titulo: "Você encontrou o Kaça", imagem: self.kacaImage!)
+                } else if imageView.image == self.roulleteResultImages[1]{
+                    self.createAlert(titulo: "Você encontrou a Elo", imagem: self.eloImage!)
+                } else if imageView.image == self.roulleteResultImages[2]{
+                    self.createAlert(titulo: "Você encontrou o Fabricio", imagem: self.fabricioImage!)
+                } else if imageView.image == self.roulleteResultImages[3]{
+                    self.createAlert(titulo: "Você encontrou o Joao", imagem: self.joaoImage!)
+                } else if imageView.image == self.roulleteResultImages[4]{
+                    self.createAlert(titulo: "Você encontrou o Enao", imagem: self.enesImage!)
+                } else if imageView.image == self.roulleteResultImages[5]{
+                    self.createAlert(titulo: "Você encontrou o luiz", imagem: self.luizImage!)
+                } else if imageView.image == self.roulleteResultImages[6]{
+                    self.createAlert(titulo: "Você encontrou o leo", imagem: self.leoImage!)
                 } else {
-                    self.textField.text = "Não vai desistir?"
-
+                    self.createAlert(titulo: "Você encontrou o gabriel", imagem: self.gabrielImage!)
                 }
-                
-                
-                
             }
         }
-
+        
+    }
+    
+    
+    func createAlert (titulo: String, imagem: UIImage){
+        let showAlert = UIAlertController(title: titulo, message: nil, preferredStyle: .alert)
+        let imageView = UIImageView(frame: CGRect(x: 10, y: 50, width: 250, height: 230))
+        imageView.image = imagem // Your image here...
+        showAlert.view.addSubview(imageView)
+        let height = NSLayoutConstraint(item: showAlert.view, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 320)
+        let width = NSLayoutConstraint(item: showAlert.view, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 250)
+        showAlert.view.addConstraint(height)
+        showAlert.view.addConstraint(width)
+        showAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            // your actions here...
+        }))
+        
+        self.present(showAlert, animated: true, completion: nil)
+        
     }
     
     @objc func rollButtonFunc(sender: UIButton) {
@@ -105,7 +141,7 @@ class ViewController: UIViewController {
         animate(imageView: roulleteImageView, images: roulleteImages)
         
     }
-
-
+    
+    
 }
 
